5de28a716196b21cad831e70d2839e9fa220cd88
ayush1290jain <18bce082@nirmauni.ac.in>
2021-04-19 20:53:18 +0530

    image

--- IRS2021/views.py
+++ IRS2021/views.py
@@ -7,6 +7,12 @@ import regex
 import numpy as np
 import pandas as pd
 import speech_recognition as sr
+#from nudenet import NudeClassifier,NudeDetector
+#import cv2
+import math
+import argparse
+#import matplotlib.pyplot as plt
+#import matplotlib.image as img
 
 
 # Create your views here.
@@ -213,5 +219,122 @@ def speech1(request):
         print('Your voice not recognised')
     return render(request, 'speech.html',{'probability':p1,'adults': adults , 'words':tot})
 
+'''def highlightFace(net, frame, conf_threshold=0.7):
+    frameOpencvDnn=frame.copy()
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections=net.forward()
+    faceBoxes=[]
+    for i in range(detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence>conf_threshold:
+            x1=int(detections[0,0,i,3]*frameWidth)
+            y1=int(detections[0,0,i,4]*frameHeight)
+            x2=int(detections[0,0,i,5]*frameWidth)
+            y2=int(detections[0,0,i,6]*frameHeight)
+            faceBoxes.append([x1,y1,x2,y2])
+            #cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn,faceBoxes
+
+def age_detect():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--image')
+
+    args=parser.parse_args()
+
+    faceProto="opencv_face_detector.pbtxt"
+    faceModel="opencv_face_detector_uint8.pb"
+    ageProto="age_deploy.prototxt"
+    ageModel="age_net.caffemodel"
+    genderProto="gender_deploy.prototxt"
+    genderModel="gender_net.caffemodel"
+
+    MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+    ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    mapping={'(0-2)':0, '(4-6)':1, '(8-12)':2, '(15-20)':3, '(25-32)':4, '(38-43)':5, '(48-53)':6, '(60-100)':7}
+    genderList=['Male','Female']
+
+    faceNet=cv2.dnn.readNet(faceModel,faceProto)
+    ageNet=cv2.dnn.readNet(ageProto,ageModel)
+    genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+    cam=cv2.VideoCapture(0)
+    padding=20
+    i=0
+    hasFrame=''
+    frame=''
+
+    while True:
+        hasFrame,frame=cam.read()
+        if not hasFrame:
+            cv2.waitKey()
+            break
+        break
+
+    resultImg,faceBoxes=highlightFace(faceNet,frame)
+
+    if not faceBoxes:
+        print("No face detected")
+
+    for faceBox in faceBoxes:
+        face=frame[max(0,faceBox[1]-padding):
+                   min(faceBox[3]+padding,frame.shape[0]-1),max(0,faceBox[0]-padding)
+                   :min(faceBox[2]+padding, frame.shape[1]-1)]
+
+        blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds=genderNet.forward()
+        gender=genderList[genderPreds[0].argmax()]
+        print(f'Gender: {gender}')
+
+        ageNet.setInput(blob)
+        agePreds=ageNet.forward()
+        age=ageList[agePreds[0].argmax()]
+        print(f'Age: {age[1:-1]} years')
+
+        #cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+        #cv2.imshow("Detecting age and gender", resultImg)
+        #key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        #if key == ord("q"):
+        #   break
+
+        if(mapping[age]<=3):
+            return False
+        else:
+            return True
+'''
+
 def image1(request):
-    return render(request, 'text.html')
+
+    if request.method == 'POST':
+        d = request.FILES['text_submit'].readlines()
+    '''img1 = 'girl2.jpeg'
+    testImage = img.imread(img1)
+
+    plt.imshow(testImage)
+    # plt.show()
+
+    classifier = NudeClassifier()
+
+    # A. Classify single image
+    di_out = classifier.classify(testImage)
+    print(di_out)
+    threshold = 0.5
+
+    text=''
+    if di_out[0]['unsafe'] < threshold:
+        plt.imshow(testImage)
+        # plt.show()
+    else:
+        age = age_detect()
+        if age == True:
+            #text='You can view the adult image'
+            plt.imshow(testImage)
+            # plt.show()
+        else:
+            text="Sorry! You can't view the adult image"'''
+    return render(request,'image.html')
