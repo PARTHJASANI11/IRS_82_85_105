16af3a72b52b26bd169bb853a6ff0abbd4b80222
ayush1290jain <18bce082@nirmauni.ac.in>
2021-04-23 16:11:35 +0530

    image_classification_done image_detection_half

--- IRS2021/views.py
+++ IRS2021/views.py
@@ -9,10 +9,10 @@ import pandas as pd
 import speech_recognition as sr
 import cv2
 import math
-from nudenet import NudeClassifier
+from nudenet import NudeClassifier,NudeDetector
 import argparse
-#import matplotlib.pyplot as plt
-#import matplotlib.image as img
+import matplotlib.pyplot as plt
+import matplotlib.image as img
 from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
@@ -220,89 +220,88 @@ def speech1(request):
     return render(request, 'speech.html',{'probability':p1,'adults': adults , 'words':tot})
 
 def highlightFace(net, frame, conf_threshold=0.7):
-    frameOpencvDnn=frame.copy()
-    frameHeight=frameOpencvDnn.shape[0]
-    frameWidth=frameOpencvDnn.shape[1]
-    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
 
     net.setInput(blob)
-    detections=net.forward()
-    faceBoxes=[]
+    detections = net.forward()
+    faceBoxes = []
+    print(detections.shape)
     for i in range(detections.shape[2]):
-        confidence=detections[0,0,i,2]
-        if confidence>conf_threshold:
-            x1=int(detections[0,0,i,3]*frameWidth)
-            y1=int(detections[0,0,i,4]*frameHeight)
-            x2=int(detections[0,0,i,5]*frameWidth)
-            y2=int(detections[0,0,i,6]*frameHeight)
-            faceBoxes.append([x1,y1,x2,y2])
-            #cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
-    return frameOpencvDnn,faceBoxes
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            faceBoxes.append([x1, y1, x2, y2])
+            # cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, faceBoxes
+
 
 def age_detect():
-    parser=argparse.ArgumentParser()
-    parser.add_argument('--image')
 
-    args=parser.parse_args()
+    faceProto = "IRS2021/opencv_face_detector.pbtxt"
+    faceModel = "IRS2021/opencv_face_detector_uint8.pb"
+    ageProto = "IRS2021/age_deploy.prototxt"
+    ageModel = "IRS2021/age_net.caffemodel"
+    genderProto = "IRS2021/gender_deploy.prototxt"
+    genderModel = "IRS2021/gender_net.caffemodel"
 
-    faceProto="opencv_face_detector.pbtxt"
-    faceModel="opencv_face_detector_uint8.pb"
-    ageProto="age_deploy.prototxt"
-    ageModel="age_net.caffemodel"
-    genderProto="gender_deploy.prototxt"
-    genderModel="gender_net.caffemodel"
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+    ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    mapping = {'(0-2)': 0, '(4-6)': 1, '(8-12)': 2, '(15-20)': 3, '(25-32)': 4, '(38-43)': 5, '(48-53)': 6,
+               '(60-100)': 7}
+    genderList = ['Male', 'Female']
 
-    MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
-    ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-    mapping={'(0-2)':0, '(4-6)':1, '(8-12)':2, '(15-20)':3, '(25-32)':4, '(38-43)':5, '(48-53)':6, '(60-100)':7}
-    genderList=['Male','Female']
+    faceNet = cv2.dnn.readNet(faceModel, faceProto)
+    ageNet = cv2.dnn.readNet(ageProto, ageModel)
+    genderNet = cv2.dnn.readNet(genderModel, genderProto)
 
-    faceNet=cv2.dnn.readNet(faceModel,faceProto)
-    ageNet=cv2.dnn.readNet(ageProto,ageModel)
-    genderNet=cv2.dnn.readNet(genderModel,genderProto)
-
-    cam=cv2.VideoCapture(0)
-    padding=20
-    i=0
-    hasFrame=''
-    frame=''
+    cam = cv2.VideoCapture(0)
+    padding = 20
+    i = 0
+    hasFrame = ''
+    frame = ''
 
     while True:
-        hasFrame,frame=cam.read()
+        hasFrame, frame = cam.read()
         if not hasFrame:
             cv2.waitKey()
             break
         break
 
-    resultImg,faceBoxes=highlightFace(faceNet,frame)
+    resultImg, faceBoxes = highlightFace(faceNet, frame)
 
     if not faceBoxes:
         print("No face detected")
 
     for faceBox in faceBoxes:
-        face=frame[max(0,faceBox[1]-padding):
-                   min(faceBox[3]+padding,frame.shape[0]-1),max(0,faceBox[0]-padding)
-                   :min(faceBox[2]+padding, frame.shape[1]-1)]
+        face = frame[max(0, faceBox[1] - padding):
+                     min(faceBox[3] + padding, frame.shape[0] - 1), max(0, faceBox[0] - padding)
+                                                                    :min(faceBox[2] + padding, frame.shape[1] - 1)]
 
-        blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
         genderNet.setInput(blob)
-        genderPreds=genderNet.forward()
-        gender=genderList[genderPreds[0].argmax()]
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
         print(f'Gender: {gender}')
 
         ageNet.setInput(blob)
-        agePreds=ageNet.forward()
-        age=ageList[agePreds[0].argmax()]
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()]
         print(f'Age: {age[1:-1]} years')
 
-        #cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
-        #cv2.imshow("Detecting age and gender", resultImg)
-        #key = cv2.waitKey(1) & 0xFF
+        # cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+        # cv2.imshow("Detecting age and gender", resultImg)
+        # key = cv2.waitKey(1) & 0xFF
         # if the `q` key was pressed, break from the loop
-        #if key == ord("q"):
+        # if key == ord("q"):
         #   break
 
-        if(mapping[age]<=3):
+        if (mapping[age] <= 3):
             return False
         else:
             return True
@@ -316,22 +315,33 @@ def image1(request):
         uploaded_file_url = fs.url(filename)
         print(uploaded_file_url)
 
-    testImage = myfile
+    testImage=img.imread(myfile)
+    print('testImage: ',testImage)
+
 
     classifier = NudeClassifier()
 
     # A. Classify single image
     di_out = classifier.classify(testImage)
-    print(di_out)
     threshold = 0.5
 
     text=''
+    print('diout',di_out)
     if di_out[0]['unsafe'] < threshold:
         text=''
         #plt.imshow(testImage)
         # plt.show()
+
     else:
         age = age_detect()
+        detector = NudeDetector()
+        det_res=detector.detect(testImage)
+
+
+        for i in det_res:
+            print(i['label'])
+
+        print('age is: ',age)
         if age == True:
             text=''
             #text='You can view the adult image'
@@ -339,5 +349,7 @@ def image1(request):
             # plt.show()
         else:
             text="Sorry! You can't view the adult image"
+    if text!='':
+        uploaded_file_url=''
 
     return render(request,'image.html', {'image':uploaded_file_url})
